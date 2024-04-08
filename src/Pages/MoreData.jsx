b891b6161b956c26e0593bc424af841b9b7cc38b
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-08 19:49:17 +0200

    This project is for the Lest Gbelle Weekly Dew Payment And Records Collection

--- src/Pages/MoreData.jsx
+++ src/Pages/MoreData.jsx
@@ -1,5 +1,9 @@
 import Table from "../Components/Table/Table";
-import MEMBERS_INFO_FEBRUARY from "../Data/MemberInfo";
+import MEMBERS_INFO_JUNE from "../Data/MemberInfo";
+import MEMBERS_INFO_FEBRUARY, {
+	MEMBERS_INFO_APRIL,
+	MEMBERS_INFO_MAY,
+} from "../Data/MemberInfo";
 import MEMBERS_INFO_MARCH from "../Data/MemberInfo";
 
 const MoreData = () => {
@@ -8,8 +12,9 @@ const MoreData = () => {
 			<div className='mt-36 container mx-auto md:px-14 px-2'>
 				<Table text={"Feburay data"} month={MEMBERS_INFO_FEBRUARY} />
 				<Table text={"March data"} month={MEMBERS_INFO_MARCH} />
-				{/* <Table text={"April data"} month={""} />
-				<Table text={"May data"} month={""} /> */}
+				<Table text={"April data"} month={MEMBERS_INFO_APRIL} />
+				<Table text={"May data"} month={MEMBERS_INFO_MAY} />
+				<Table text={"June data"} month={MEMBERS_INFO_JUNE} />
 			</div>
 		</>
 	);
