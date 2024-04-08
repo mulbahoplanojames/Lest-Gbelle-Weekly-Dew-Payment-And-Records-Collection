4e817e732aa5bbef5178b07b7387e4daf1dffd27
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-08 18:33:16 +0200

    This project is for the Lest Gbelle Weekly Dew Payment And Records Collection

---
+++ src/Components/More Data/MoreData.jsx
@@ -0,0 +1,11 @@
+import Table from "../Table/Table";
+
+const MoreData = () => {
+	return (
+		<>
+			<Table />
+		</>
+	);
+};
+
+export default MoreData;
--- src/Components/Table/Table.jsx
+++ src/Components/Table/Table.jsx
@@ -5,53 +5,58 @@ import Name_Avatar from "../Name&Avatar/Name_Avatar";
 const Table = () => {
 	return (
 		<>
-			<div className='overflow-x-auto'>
-				<table className='table'>
-					{/* head */}
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Week 1</th>
-							<th>Week 2</th>
-							<th>Week 3</th>
-							<th>Week 4</th>
-						</tr>
-						.
-					</thead>
-					<tbody>
-						{MEMBERS_INFO.map((info) => (
-							<tr key={info.id}>
-								<td>
-									<Name_Avatar name={info.name} avatar={info.avatar} />
-								</td>
-								<td>
-									<CheckBoxPaid />
-								</td>
-								<td>
-									<CheckBoxPaid />
-								</td>
-								<td>
-									<CheckBoxPaid />
-								</td>
-								<td>
-									<CheckBoxPaid />
-								</td>
+			<div className=''>
+				<h1 className='pb-xl text-lg'>January Data</h1>
+				<div className='overflow-x-auto'>
+					<table className='table'>
+						{/* head */}
+						<thead>
+							<tr>
+								<th>Name</th>
+								<th>Week 1</th>
+								<th>Week 2</th>
+								<th>Week 3</th>
+								<th>Week 4</th>
 							</tr>
-						))}
-						{/* row 1 */}
-					</tbody>
+							.
+						</thead>
+						<tbody>
+							{MEMBERS_INFO.map((info) => (
+								<tr key={info.id}>
+									<td>
+										<Name_Avatar name={info.name} avatar={info.avatar} />
+									</td>
+									<td>
+										<CheckBoxPaid />
+									</td>
+									<td>
+										<CheckBoxPaid />
+									</td>
+									<td>
+										<CheckBoxPaid />
+									</td>
+									<td>
+										<CheckBoxPaid />
+									</td>
+								</tr>
+							))}
+							{/* row 1 */}
+						</tbody>
 
-					{/* foot */}
-					<tfoot>
-						<tr>
-							<th>Name</th>
-							<th>Week 1</th>
-							<th>Week 2</th>
-							<th>Week 3</th>
-							<th>Week 4</th>
-						</tr>
-					</tfoot>
-				</table>
+						{/* foot */}
+						<tfoot>
+							<tr>
+								<th>Name</th>
+								<th>Week 1</th>
+								<th>Week 2</th>
+								<th>Week 3</th>
+								<th>Week 4</th>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+
+				<h1>More Info</h1>
 			</div>
 		</>
 	);
