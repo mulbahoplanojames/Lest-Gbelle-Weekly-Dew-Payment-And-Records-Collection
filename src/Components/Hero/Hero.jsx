89afec81a824b4002f752bfbb9ba6b62cbcb4946
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-04-09 13:53:07 +0200

    Added framer motion for smooth animation

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,18 +1,36 @@
 import { Link } from "react-router-dom";
 import HERO_TEXT from "../../Constant/Constant";
 import Button from "../../Interfaces/Button/Button";
+import { motion } from "framer-motion";
 
 const Hero = () => {
 	return (
 		<>
 			<div className='mt-36 pb-1'>
-				<h1 className='text-lg opacity-60 uppercase text-transparent bg-clip-text bg-gradient-to-r from-neutral-950 via-blue-500 to-teal-500 '>
+				<motion.h1
+					whileInView={{ opacity: 1, y: 0 }}
+					initial={{ opacity: 0, y: -50 }}
+					transition={{ duration: 0.6 }}
+					className='text-lg opacity-60 uppercase text-transparent bg-clip-text bg-gradient-to-r from-neutral-950 via-blue-500 to-teal-500 '
+				>
 					LestGbelle
-				</h1>
-				<h1 className='pt-5 md:text-4xl text-3xl capitalize  lg:pr-[40rem] pr-3  '>
+				</motion.h1>
+				<motion.h1
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: -70 }}
+					transition={{ duration: 1.5 }}
+					className='pt-5 md:text-4xl text-3xl capitalize  lg:pr-[40rem] pr-3  '
+				>
 					Lest Gbelle Weekly due Payment and records Collection
-				</h1>
-				<p className='lg:pr-[40rem] pr-2 lg:pt-6 pt-3 pb-6'>{HERO_TEXT}</p>
+				</motion.h1>
+				<motion.p
+					whileInView={{ opacity: 1, x: 0 }}
+					initial={{ opacity: 0, x: -70 }}
+					transition={{ duration: 2.5 }}
+					className='lg:pr-[40rem] pr-2 lg:pt-6 pt-3 pb-6'
+				>
+					{HERO_TEXT}
+				</motion.p>
 				<Link to='/form'>
 					<Button text={"Be a Member"} />
 				</Link>
--- src/Interfaces/Button/Button.jsx
+++ src/Interfaces/Button/Button.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 const Button = ({ text }) => {
 	const handleClick = () => {
 		window.scrollTo({ top: 0, left: 0 });
@@ -5,12 +7,15 @@ const Button = ({ text }) => {
 
 	return (
 		<>
-			<button
+			<motion.button
+				whileInView={{ opacity: 1, x: 0 }}
+				initial={{ opacity: 0, x: -80 }}
+				transition={{ duration: 2 }}
 				className='py-2 px-5 bg-blue-500 text-white text-lg rounded-lg hover:bg-opacity-75'
 				onClick={handleClick}
 			>
 				{text}
-			</button>
+			</motion.button>
 		</>
 	);
 };
