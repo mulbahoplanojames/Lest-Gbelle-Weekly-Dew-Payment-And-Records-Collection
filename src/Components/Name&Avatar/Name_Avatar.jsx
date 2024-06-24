5263951b36c204545edf9b221f68aefaafdae9cf
mulbahjamesoplano <mulbahjamesoplano@gmail.com>
2024-06-24 08:39:20 +0200

    Making some changes to this site

--- src/Components/Name&Avatar/Name_Avatar.jsx
+++ src/Components/Name&Avatar/Name_Avatar.jsx
@@ -1,9 +1,16 @@
+/*
+ * This component that two props: name and avatar.
+ * The name prop is a string that represents the name of the person being displayed.
+ * This component is use with in the Table component.
+ */
+
 const Name_Avatar = ({ name, avatar }) => {
 	return (
 		<>
 			<div className='flex items-center gap-3'>
 				<div className='avatar'>
 					<div className='mask mask-squircle w-12 h-12'>
+						{/* The img element displays the avatar image. */}
 						<img src={avatar} alt={name} />
 					</div>
 				</div>
@@ -15,4 +22,5 @@ const Name_Avatar = ({ name, avatar }) => {
 	);
 };
 
+// Export the Name_Avatar component so that it can be used in other parts of the application.
 export default Name_Avatar;
